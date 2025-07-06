c7b27b7da8888084aa6d5a0a9332db86f25f7dd6
Bhoumik Patidar <140884977+BhoumikPatidar@users.noreply.github.com>
2025-07-07 02:20:13 +0530

    Update d.cpp

--- d.cpp
+++ d.cpp
@@ -1,49 +1,29 @@
-#include "x86_64ELFDynamic.h"
-#include "eld/Target/GNULDBackend.h"
-#include "x86_64LDBackend.h"
-
-#include "eld/Config/LinkerConfig.h"
-#include "eld/Target/ELFFileFormat.h"
+//===- x86_64GOT.cpp-------------------------------------------------------===//
+// Part of the eld Project, under the BSD License
+// See https://github.com/qualcomm/eld/LICENSE.txt for license information.
+// SPDX-License-Identifier: BSD-3-Clause
+//===----------------------------------------------------------------------===//
+#include "x86_64GOT.h"
+#include "eld/Readers/ELFSection.h"
+#include "eld/Readers/Relocation.h"
 
 using namespace eld;
 
-x86_64ELFDynamic::x86_64ELFDynamic(GNULDBackend &pParent, LinkerConfig &pConfig)
-    : ELFDynamic(pParent, pConfig) {}
+// GOTPLT0
+x86_64GOTPLT0 *x86_64GOTPLT0::Create(ELFSection *O, ResolveInfo *R) {
+  x86_64GOTPLT0 *G = make<x86_64GOTPLT0>(O, R);
 
-x86_64ELFDynamic::~x86_64ELFDynamic() {}
-
-void x86_64ELFDynamic::reserveTargetEntries() {
-  reserveOne(llvm::ELF::DT_RELACOUNT);
-  reserveOne(llvm::ELF::DT_JMPREL);
-  reserveOne(llvm::ELF::DT_PLTRELSZ);
-  reserveOne(llvm::ELF::DT_PLTGOT);
-  reserveOne(llvm::ELF::DT_RELA);
-  reserveOne(llvm::ELF::DT_RELASZ);
-  reserveOne(llvm::ELF::DT_RELAENT);
-}
-
-void x86_64ELFDynamic::applyTargetEntries() {
-  uint32_t relaCount = 0;
-  for (auto &R : m_Backend.getRelaDyn()->getRelocations()) {
-    if (R->type() == llvm::ELF::R_X86_64_RELATIVE)
-      relaCount++;
-  }
-  applyOne(llvm::ELF::DT_RELACOUNT, relaCount);
-
-  // Add PLT-related dynamic entries
-  if (m_Backend.getRelaPLT()->size() > 0) {
-    applyOne(llvm::ELF::DT_JMPREL, m_Backend.getRelaPLT()->addr());
-    applyOne(llvm::ELF::DT_PLTRELSZ, m_Backend.getRelaPLT()->size());
+  if (R) {
+    // GOT[0] = &_DYNAMIC - Create relocation for _DYNAMIC symbol
+    Relocation *r1 = Relocation::Create(llvm::ELF::R_X86_64_64, 64,
+                                        make<FragmentRef>(*G, 0), 0);
+    r1->setSymInfo(R);
+    O->addRelocation(r1);
   }
 
-  if (m_Backend.getGOTPLT()) {
-    applyOne(llvm::ELF::DT_PLTGOT, m_Backend.getGOTPLT()->addr());
-  }
+  // GOT[1] and GOT[2] are filled by dynamic linker at runtime:
+  // GOT[1] = link_map pointer 
+  // GOT[2] = resolver function address
 
-  // Add general relocation entries
-  if (m_Backend.getRelaDyn()->size() > 0) {
-    applyOne(llvm::ELF::DT_RELA, m_Backend.getRelaDyn()->addr());
-    applyOne(llvm::ELF::DT_RELASZ, m_Backend.getRelaDyn()->size());
-    applyOne(llvm::ELF::DT_RELAENT, m_Backend.getRelaEntrySize());
-  }
+  return G;
 }
